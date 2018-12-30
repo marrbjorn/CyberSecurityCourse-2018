f7b894ba0f76d6c047fc277dee99cf6865c5d759
Marrbjörn <dreamr@protonmail.ch>
2018-12-30 03:12:16 +0300

    Update SignupRepository.java

--- src/main/java/sec/project/repository/SignupRepository.java
+++ src/main/java/sec/project/repository/SignupRepository.java
@@ -1,1 +1,6 @@
+package sec.project.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import sec.project.domain.Signup;
+
+public interface SignupRepository extends JpaRepository<Signup, Long> {}
