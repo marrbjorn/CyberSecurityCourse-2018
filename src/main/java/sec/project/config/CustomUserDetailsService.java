708af216314e05dc9758ddd6d154e52e34e6fc82
Marrbjörn <dreamr@protonmail.ch>
2018-12-30 05:16:38 +0300

    Update SecurityConfiguration.java and add one file

---
+++ src/main/java/sec/project/config/CustomUserDetailsService.java
@@ -0,0 +1,60 @@
+﻿package sec.project.config;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.annotation.PostConstruct;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private Map<String, String> accountDetails;
+
+    @PostConstruct
+    public void init() {
+        this.accountDetails = new TreeMap<>();
+        this.accountDetails.put("Valravn","(+F?%2^hXV=)2PbE@&;dJ4D]{SkZ_`Sf94n%S]uaNVP>;2w<sB?2^g");
+        this.accountDetails.put("Wardruna","fh2T[_6H h9ZfWn%pFxA_UPsFA^ym2`N+{3ge_'2&ffV~dFVAP*}Q/M");
+        this.accountDetails.put("Ilmarinen",";.;vmRh>Xd'5<r[5uBHgcHs3G`c7WBRe3*%Tw]y&'cDL-Qe<HkV:Z9#");
+        this.accountDetails.put("Karhu","d'Y].=;msd:)fg)a<dzN{V5bj.S_fAe4&)U*/~m>Y,37>}3K'jb47^fFdds*m");
+        this.accountDetails.put("Asgeir","Fylkir");
+        this.accountDetails.put("Eivør","Sinklars Visa");
+        this.accountDetails.put("Harkaliðið","Í Dans");
+        this.accountDetails.put("Skogvidr","D%GfS_s5{@2{,U9LBc!wv7XG4U5B&8Vgr?Y;:;&4+34(>ShTY'VkS89Q");
+        this.accountDetails.put("admin","admin");
+        this.accountDetails.put("CMS","CMSpassword");
+      //      ^ we are able to remove this "debug" password for "admin"-access;
+     //      and to remove latest password which can be as "backdoor" from "tools in use";
+    // but it is also good to use "encrypt"-methods for storing passwords in database;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (!this.accountDetails.containsKey(username)) {
+            throw new UsernameNotFoundException("No such user: " + username);
+        }
+        if (this.accountDetails.get(username).equals("admin")){
+        return new org.springframework.security.core.userdetails.User(
+             username,
+                this.accountDetails.get(username),
+                true,
+                true,
+                true,
+                true,
+                Arrays.asList(new SimpleGrantedAuthority("ADMIN")));
+        }
+        return new org.springframework.security.core.userdetails.User(
+                username,
+                this.accountDetails.get(username),
+                true,
+                true,
+                true,
+                true,
+                Arrays.asList(new SimpleGrantedAuthority("USER")));
+    }
+}
--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -1,1 +1,80 @@
+﻿package sec.project.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+      // if we want to use encrypt-method for passwords - required to use next import-strings:
+     // import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    //  import org.springframework.security.crypto.password.PasswordEncoder;
+   //   import org.springframework.context.annotation.Bean;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+      // with this ^ setting there will be "csrf-token as cookie";
+     //   It is can be OK, but there is also option for HttpOnly as "false()";
+    //   and, as a result, if will be possible to do cross-site-scripting...
+   //      will be possible to get this csrf-token by the scripts.
+  //    For potential fix: entire string can be "removed" for enabling "normal" CSRF-protection..
+ //        or if such design is required indeed - to configure it properly and with security-meanings!
+        http.cors().disable();
+        http.headers().defaultsDisabled().contentTypeOptions();
+      //   ^ with not disabled option (or custom-setting) headers can be next:
+     // https://docs.spring.io/spring-security/site/docs/current/reference/html/headers.html
+    //     where also visible other options to configure headers-settings when required custom design.
+   //   so, possible to add something more tricky than just to disable entire feature;
+  //  there we tried to disable 'Content Type Options'-feature; As a result, potential content sniffing is valid.    
+ //       and disabled CORS is just as addition to points which should be enabled in fact;
+//    (31-32) Strings possible to remove for "get back" default "proper" security features!
+        http.authorizeRequests()
+                .antMatchers("/hidden", "/css/**").permitAll()
+      //   we are able to remove ^ this "/hidden"-entry for remove "debug"-access (when application is released);
+     //    and even more - good to remove entire html-page from released-version.
+                .antMatchers("/formsrc").hasAuthority("ADMIN")
+      //  possible to use such authority ^ checks for all pages where expected access only by admins. 
+     //   for example, to "/fylkr"-resource.        
+                .anyRequest().authenticated();
+        http.formLogin().permitAll();
+      //  but ^ good to think about logout too; currently, logout is not implemented at all;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+/* sidenote:
+there is try to use encryption, but it is clear that md4 is not considered secure.
+in addition, is not salted enough. Even if such encryption will be used - it is not secure. 
+
+to change string from this method to next string:
+auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+and to implement anothermethod:
+    @Bean
+    public PasswordEncoder passwordEncoder()
+                                    {return new Md4PasswordEncoder();}
+
+then will be example of normal encryption, but good to add more tweaks.
+*/
+
+    }
+}
+/*
+      // We are able to add this method for encrypting passwords;
+     // In addition to this method we have to to re-design previous method too:
+    // auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+   
+    @Bean
+    public PasswordEncoder passwordEncoder()
+                                    {return new BCryptPasswordEncoder();}
+*/
